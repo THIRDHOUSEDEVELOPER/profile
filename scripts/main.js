9298943537f7dd3daf51a145ee9e5fc58951cc3a
thirdhousedeveloper <enockson11@gmail.com>
2022-12-13 15:15:34 +0100

    Add files via push

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+=======
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+$(document).ready(function() {
+  $('#about, #about1').hide();
+  $('#showdetail').click(function() {
+      $('#about, #about1').toggle("slide");
+  });
+});
+>>>>>>> my second commit
