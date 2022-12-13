8b0f5a35b4130cc5e5369a8e854c841a879a3518
thirdhousedeveloper <enockson11@gmail.com>
2022-12-13 14:28:16 +0100

    my second commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Animations
 AOS.init({
   anchorPlacement: 'top-left',
@@ -5,3 +6,18 @@ AOS.init({
 });
 
 // Add your javascript here
+=======
+// Animations
+AOS.init({
+  anchorPlacement: 'top-left',
+  duration: 1000
+});
+
+// Add your javascript here
+$(document).ready(function() {
+  $('#about, #about1').hide();
+  $('#showdetail').click(function() {
+      $('#about, #about1').toggle("slide");
+  });
+});
+>>>>>>> my second commit
